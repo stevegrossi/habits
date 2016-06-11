3787401d4aab8e5044f2f234aed6d7407b41ffd8
Steve Grossi <me@stevegrossi.com>
2016-06-11 10:57:32 -0400

    Initial layout and CSS

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Habits.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Habits!"
   end
 end
