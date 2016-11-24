b44ce90b9e19a5eff0cfee3142d1c45c1923d973
Steve Grossi <me@stevegrossi.com>
2016-11-24 14:08:36 -0500

    Whoops, don’t make everyone use habits.stevegrossi.com

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :habits, Habits.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "http", host: "habits.stevegrossi.com", port: 80],
+  url: [scheme: "http", host: {:system, "HOSTNAME"}, port: 80],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
