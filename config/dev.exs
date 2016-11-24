ae4f250e55ce54a4bbf374cd0720cb8d1326d50f
Steve Grossi <me@stevegrossi.com>
2016-11-24 14:08:24 -0500

    Use default SECRET_KEY_BASE in dev

--- config/dev.exs
+++ config/dev.exs
@@ -7,6 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :habits, Habits.Endpoint,
+  secret_key_base: "0000000000000000000000000000000000000000000000000000000000000000",
   http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
