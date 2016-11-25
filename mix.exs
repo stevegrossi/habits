cdc4529846fad04ac908a5b91a1407495da48d8a
Steve Grossi <me@stevegrossi.com>
2016-11-25 07:39:42 -0500

    Upgrade phoenix

--- mix.exs
+++ mix.exs
@@ -36,9 +36,9 @@ defmodule Habits.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.2.0-rc"},
-      {:phoenix_pubsub, "~> 1.0.0-rc"},
-      {:phoenix_ecto, "~> 3.0-rc"},
+      {:phoenix, "~> 1.2.1"},
+      {:phoenix_pubsub, "~> 1.0.1"},
+      {:phoenix_ecto, "~> 3.0.1"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.5"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
