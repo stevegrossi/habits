ea86f94f8f4fda28af8cd39b11f8b69cbd756e25
Steve Grossi <me@stevegrossi.com>
2016-11-26 06:54:14 -0500

    Set date when initializing App

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -5,15 +5,9 @@ import App from "./components/App"
 const habitContainer = document.getElementById("habit-container")
 if (habitContainer) {
   const habits = JSON.parse(habitContainer.dataset.habits)
-  const [_empty, year, month, day] = window.location.pathname.split("/")
-  const date = new Date(
-    parseInt(year),
-    parseInt(month) - 1,
-    parseInt(day)
-  )
 
   ReactDOM.render(
-    <App date={date} data={habits} />,
+    <App data={habits} />,
     habitContainer
   )
 }
--- web/static/js/components/App.js
+++ web/static/js/components/App.js
@@ -4,11 +4,24 @@ import DateNav from "./DateNav"
 import HabitList from "./HabitList"
 
 class App extends React.Component {
+  constructor(args) {
+    super(args)
+
+    const [_empty, year, month, day] = window.location.pathname.split("/")
+    const date = new Date(
+      parseInt(year),
+      parseInt(month) - 1,
+      parseInt(day)
+    )
+    this.state = {
+      date: date
+    }
+  }
 
   render() {
     return (
     <div>
-      <DateNav date={this.props.date} />
+      <DateNav date={this.state.date} />
       <HabitList data={this.props.data} />
     </div>
     )
