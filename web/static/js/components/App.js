8c0f9ab6efe7cf0dcdf41978cf24e6dacc95edc4
Steve Grossi <me@stevegrossi.com>
2016-11-27 14:01:46 -0500

    Oh yeah, just always use current date on first load

--- web/static/js/components/App.js
+++ web/static/js/components/App.js
@@ -6,27 +6,18 @@ import HabitList from "./HabitList"
 class App extends React.Component {
   constructor(args) {
     super(args)
-
-    const [_empty, year, month, day] = window.location.pathname.split("/")
-    const date = new Date(
-      parseInt(year),
-      parseInt(month) - 1,
-      parseInt(day)
-    )
-    this.state = { date: date }
+    this.state = { date: new Date() }
   }
 
   goToPrev() {
     const date = new Date(this.state.date)
     const previousDay = new Date(date.setDate(date.getDate() - 1));
-
     this.setState({ date: previousDay })
   }
 
   goToNext() {
     const date = new Date(this.state.date)
     const nextDay = new Date(date.setDate(date.getDate() + 1));
-
     this.setState({ date: nextDay })
   }
 
