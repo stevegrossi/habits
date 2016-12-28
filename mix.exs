02aa861a0ec1a7cb2b3d2c0d037b396fcadca81b
Steve Grossi <me@stevegrossi.com>
2016-12-28 06:55:48 -0500

    Specify correct Elixir version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Habits.Mixfile do
   def project do
     [app: :habits,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.3.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
