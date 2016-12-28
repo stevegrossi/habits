22cdf50088b17e21e0679df7145aa99536706828
Steve Grossi <me@stevegrossi.com>
2016-12-28 05:40:36 -0500

    Refactor API.V1.HabitController

--- web/controllers/api/v1/habit_controller.ex
+++ web/controllers/api/v1/habit_controller.ex
@@ -1,25 +1,37 @@
 defmodule Habits.API.V1.HabitController do
   use Habits.Web, :controller
 
-  alias Habits.Session
   alias Habits.CheckIn
   alias Habits.Habit
 
-  def index(conn, %{"date" => date_string}) do
-    {:ok, date} = Timex.parse(date_string, "%F", :strftime)
+  @doc """
+  Override action/2 to provide current_account to actions
+  """
+  def action(conn, _) do
+    args = [conn, conn.params, conn.assigns[:current_account]]
+    apply(__MODULE__, action_name(conn), args)
+  end
+
+  @doc """
+  Return all of the current account’s habits for the given date as JSON
+  """
+  def index(conn, %{"date" => date_string}, current_account) do
+    date = date_string_to_date(date_string)
     habits =
-      Session.current_account(conn)
+      current_account
       |> assoc(:habits)
       |> Repo.all
 
     render conn, "index.json", habits: habits, date: date
   end
 
-  def check_in(conn, %{"habit_id" => habit_id, "date" => date_string}) do
+  @doc """
+  Create a CheckIn for the given date and habit, unless one exists.
+  """
+  def check_in(conn, %{"habit_id" => habit_id, "date" => date_string}, current_account) do
     date = date_string_to_date(date_string)
 
-    with account <- Session.current_account(conn),
-         {:ok, habit} <- Habit.get_by_account(account, habit_id),
+    with {:ok, habit} <- Habit.get_by_account(current_account, habit_id),
          {:ok, check_in} <- CheckIn.create_for_date(habit, date) do
 
       render conn, "show.json", habit: habit, date: date_string
@@ -31,11 +43,13 @@ defmodule Habits.API.V1.HabitController do
     end
   end
 
-  def check_out(conn, %{"habit_id" => habit_id, "date" => date_string}) do
+  @doc """
+  Delete the CheckIn for the given date and habit.
+  """
+  def check_out(conn, %{"habit_id" => habit_id, "date" => date_string}, current_account) do
     date = date_string_to_date(date_string)
 
-    with account <- Session.current_account(conn),
-         {:ok, habit} <- Habit.get_by_account(account, habit_id),
+    with {:ok, habit} <- Habit.get_by_account(current_account, habit_id),
          {:ok, check_in} <- CheckIn.get_by_date(habit, date) do
 
       Habits.Repo.delete!(check_in)
