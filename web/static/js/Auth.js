3c532b646b828ba332cedd6b331cbe911b5cde5b
Steve Grossi <me@stevegrossi.com>
2017-01-01 06:49:23 -0500

    Get a token upon logging in

--- web/static/js/Auth.js
+++ web/static/js/Auth.js
@@ -1,19 +1,48 @@
 const Auth = {
 
   login: function(email, password) {
-    console.log("Log In:", email, password)
-    // make request
-    localStorage.setItem('token', '1234')
-    return true
+    this.requestLogin(email, password, (token) => {
+      if (token) {
+        localStorage.setItem('token', token)
+        return true
+      } else {
+        return false
+      }
+    })
   },
 
   logout: function() {
+    // destroy token on server
     localStorage.removeItem('token')
     return true
   },
 
   isLoggedIn: function() {
     return !!localStorage.getItem('token')
+  },
+
+  requestLogin: function(email, password, callback) {
+    fetch('/api/v1/sessions', {
+      method: 'post',
+      credentials: 'include',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        account: {
+          email: email,
+          password: password
+        }
+      })
+    })
+    .then(function(response) {
+      return response.json()
+    }).then(function(json) {
+      callback(json.data.token)
+    }).catch(function(error) {
+      console.error('Error fetching JSON:', error)
+    })
   }
 }
 
