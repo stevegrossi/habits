6adb5aeea77a01d03006a1323f71ecd803d53905
Steve Grossi <me@stevegrossi.com>
2017-02-11 10:45:49 -0500

    Extract icons into component
    Not perfect (I wish they all had the same view box)

--- web/static/js/components/HabitPage.js
+++ web/static/js/components/HabitPage.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { browserHistory, Link } from 'react-router'
 import Request from '../Request'
 import Loading from './Loading'
+import Icon from './Icon'
 
 class HabitPage extends React.Component {
 
@@ -72,14 +73,10 @@ class HabitPage extends React.Component {
             <div className="InlineForm">
               <input className="InlineForm-input TextInput" ref={(node) => this.nameInput = node } defaultValue={data.name} type="text" autoFocus onFocus={this.selectOnFocus} />
               <button className="InlineForm-button" onClick={this.submitEdit}>
-                <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 24 24">
-                  <path d="M21 5q.43 0 .715.285T22 6q0 .422-.289.711l-12 12Q9.422 19 9 19t-.711-.289l-6-6Q2 12.422 2 12q0-.43.285-.715T3 11q.422 0 .711.289L9 16.586 20.289 5.289Q20.578 5 21 5z"/>
-                </svg>
+                <Icon name="accept" />
               </button>
               <button className="InlineForm-button" onClick={this.cancelEdit}>
-                <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 24 24">
-                  <path d="M19 4q.43 0 .715.285T20 5q0 .422-.289.711L13.414 12l6.297 6.289Q20 18.578 20 19q0 .43-.285.715T19 20q-.422 0-.711-.289L12 13.414l-6.289 6.297Q5.422 20 5 20q-.43 0-.715-.285T4 19q0-.422.289-.711L10.586 12 4.289 5.711Q4 5.422 4 5q0-.43.285-.715T5 4q.422 0 .711.289L12 10.586l6.289-6.297Q18.578 4 19 4z"/>
-                </svg>
+                <Icon name="cancel" />
               </button>
             </div>
           }
@@ -87,9 +84,7 @@ class HabitPage extends React.Component {
             <h2 className="mt0">
               {data.name}
               <a href="#" onClick={this.handleEdit}>
-                <svg className="InlineIcon muted" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-                  <path d="M15 16H1a1 1 0 0 1-1-1V1a1 1 0 0 1 1-1h10L9 2H2v12h12V7l2-2v10a1 1 0 0 1-1 1zM4 12V9l9-9h1l2 2v1l-9 9H4zm4.5-3.5L14 3l-1-1-5.5 5.5 1 1zM6 9l-1 1v1h1l1-1-1-1z"/>
-                </svg>
+                <Icon className="Icon--inline muted" name="edit" />
               </a>
             </h2>
           }
@@ -97,9 +92,7 @@ class HabitPage extends React.Component {
           <p className="Metric">
             <span className="Metric-title">Current Streak</span>
             <span className="Metric-number">
-              <svg className="StreakIcon" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="14" height="28" viewBox="0 0 14 28">
-                <path d="M13.828 8.844c0.172 0.187 0.219 0.453 0.109 0.688l-8.437 18.078c-0.125 0.234-0.375 0.391-0.656 0.391-0.063 0-0.141-0.016-0.219-0.031-0.344-0.109-0.547-0.438-0.469-0.766l3.078-12.625-6.344 1.578c-0.063 0.016-0.125 0.016-0.187 0.016-0.172 0-0.359-0.063-0.484-0.172-0.187-0.156-0.25-0.391-0.203-0.609l3.141-12.891c0.078-0.297 0.359-0.5 0.688-0.5h5.125c0.391 0 0.703 0.297 0.703 0.656 0 0.094-0.031 0.187-0.078 0.281l-2.672 7.234 6.188-1.531c0.063-0.016 0.125-0.031 0.187-0.031 0.203 0 0.391 0.094 0.531 0.234z"></path>
-              </svg>
+              <Icon name="streak" />
               {Number(data.currentStreak).toLocaleString()}
             </span>
           </p>
