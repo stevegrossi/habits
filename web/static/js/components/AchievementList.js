c05e666347e984b6ed3195a45345e3ed760ff70a
Steve Grossi <me@stevegrossi.com>
2017-03-05 06:43:56 -0500

    Show overall achievement completion ratio

--- web/static/js/components/AchievementList.js
+++ web/static/js/components/AchievementList.js
@@ -21,10 +21,25 @@ class AchievementList extends React.Component {
     })
   }
 
+  completedCount() {
+    return this.state.achievements.filter((achievement) =>
+      achievement.value >= achievement.threshold
+    ).length;
+  }
+
+  totalCount() {
+    return this.state.achievements.length;
+  }
+
   render() {
     return (
       <div className="AchievementList">
-        <h2 className="h2 ta-c">Achievements</h2>
+        <h2 className="h2 ta-c">
+          Achievements
+          {this.state.achievements &&
+            <span>&nbsp;({this.completedCount()}/{this.totalCount()})</span>
+          }
+        </h2>
         <ul className="AchievementList-list">
           {this.state.achievements && this.state.achievements.map((achievement) =>
             <Achievement key={achievement.name} achievement={achievement} />
