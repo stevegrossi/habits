3b1fd4b92f4be127b18ff147d44dccdcc866da53
Steve Grossi <me@stevegrossi.com>
2017-03-19 09:21:50 -0400

    Add Promise polypill for phantomjs
    https://github.com/github/fetch#installation

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import AllHabits from './components/AllHabits'
 import App from './components/App'
+import Promise from 'promise-polyfill';
+
+// To add to window
+if (!window.Promise) {
+  window.Promise = Promise;
+}
 
 const habitContainer = document.getElementById('habit-container')
 if (habitContainer) {
