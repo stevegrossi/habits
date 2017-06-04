8c837779e21c245391dc7d90ba3f33141b5633e5
Steve Grossi <me@stevegrossi.com>
2017-06-04 11:37:00 -0400

    Have Wallaby fail on JS errors
    The README makes it sound like this is the default (they say to set it to `false` if you *don’t* want it to fail on JS errors) but I’m seeing I need to set it to `true` to see JS errors.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -3,4 +3,5 @@ ExUnit.start
 Ecto.Adapters.SQL.Sandbox.mode(Habits.Repo, :manual)
 
 Application.put_env(:wallaby, :base_url, Habits.Endpoint.url)
+Application.put_env(:wallaby, :js_errors, true)
 {:ok, _} = Application.ensure_all_started(:wallaby)
