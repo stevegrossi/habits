f958c2b888e0f9239b33fe7efbaae771ee1106ac
Steve Grossi <me@stevegrossi.com>
2017-07-02 18:26:05 -0400

    Let’s forget about wallaby JS errors for now
    A React PropTypes deprecation warning for a library I’m using is setting this off. I’ll update that separately.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -3,5 +3,5 @@ ExUnit.start
 Ecto.Adapters.SQL.Sandbox.mode(Habits.Repo, :manual)
 
 Application.put_env(:wallaby, :base_url, Habits.Endpoint.url)
-Application.put_env(:wallaby, :js_errors, true)
+Application.put_env(:wallaby, :js_errors, false)
 {:ok, _} = Application.ensure_all_started(:wallaby)
