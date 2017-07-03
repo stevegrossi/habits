eac213e79e183e9929fb3e6e09543c4827b46cf6
Steve Grossi <me@stevegrossi.com>
2017-07-03 09:07:29 -0400

    Update ex_machina to 2.0

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule Habits.Mixfile do
       {:geoip, "~> 0.1"},
       {:plug_x_forwarded_for, "~> 0.1"},
       {:mix_test_watch, "~> 0.2", only: :dev},
-      {:ex_machina, "~> 1.0", only: :test},
+      {:ex_machina, "~> 2.0", only: :test},
       {:credo, "~> 0.5", only: :dev},
       {:wallaby, "~> 0.16", only: :test}
     ]
