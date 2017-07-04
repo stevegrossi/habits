f79d8b35bea5eb2ca24a88550f27012c08a4c6bf
Steve Grossi <me@stevegrossi.com>
2017-07-04 10:46:45 -0400

    Test managing My Account and sessions

---
+++ test/acceptance/account_management_test.exs
@@ -0,0 +1,35 @@
+defmodule Habits.AccountManagementTest do
+  use Habits.AcceptanceCase
+
+  setup _context do
+    account = Factory.insert(:account,
+      encrypted_password: Comeonin.Bcrypt.hashpwsalt("password")
+    )
+    habit = Factory.insert(:habit, account: account)
+    Factory.insert(:check_in, habit: habit)
+    Factory.insert(:session, account: account, location: "Area 51")
+
+    [account: account]
+  end
+
+  test "My Account page and active sessions",
+    %{session: session, account: account} do
+
+    session
+    |> visit("/")
+    |> click(link("Log In"))
+    |> fill_in(text_field("Email"), with: account.email)
+    |> fill_in(text_field("Password"), with: "password")
+    |> click(button("Log In"))
+    |> click(link("Me"))
+    |> assert_has(css("h2", text: account.email))
+    |> assert_has(css(".fs-xl", text: "1")) # Total Check-Ins
+    |> assert_has(css(".AchievementList"))
+    |> click(link("Active Sessions"))
+    |> take_screenshot
+    |> assert_has(css(".ActionList-item", text: "Earth (Current)"))
+    |> find(css(".ActionList-item", text: "Area 51"))
+    |> click(link("×"))
+    |> refute_has(css(".ActionList-item", text: "Area 51"))
+  end
+end
