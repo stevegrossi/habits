0f9d200c38682b1f88fef304a25ca895876ec896
Steve Grossi <me@stevegrossi.com>
2017-07-28 20:28:56 -0400

    Bump Phoenix to 1.3
    Thanks Chris! https://gist.github.com/chrismccord/71ab10d433c98b714b75c886eff17357

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :habits, Habits.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: {:system, "HOSTNAME"}, port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
